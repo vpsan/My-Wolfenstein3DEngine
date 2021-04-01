7d2d1d00de9012fbb05927478998b725364bbf19
vpsan <valery.sandul@gmail.com>
2021-04-01 22:50:50 +0300

    12.4 Rcstng refactored in one several functions.

--- srcs/game/rcstng.c
+++ srcs/game/rcstng.c
@@ -48,6 +48,27 @@ int 	rcstng_calculations(t_game *cube)
 		cube->rcstg.draw_end = cube->map_prmtrs.win_height - 1;
 }
 
+int		rcstng_calculate_wallx(t_game *cube)
+{
+	if (cube->rcstg.side == 0)
+		cube->rcstg.wall_x = cube->plr.pos_y + cube->rcstg.perp_wall_dist *
+											   cube->rcstg.ray_dir_y;
+	else
+		cube->rcstg.wall_x = cube->plr.pos_x + cube->rcstg.perp_wall_dist *
+											   cube->rcstg.ray_dir_x;
+	cube->rcstg.wall_x = 1 - (cube->rcstg.wall_x - floor(cube->rcstg.wall_x));
+
+	cube->rcstg.tex_x = (int)(cube->rcstg.wall_x * cube->rcstg.tex->width);
+	if (cube->rcstg.side == 0 && cube->rcstg.ray_dir_x > 0)
+		cube->rcstg.tex_x = cube->rcstg.tex->width - cube->rcstg.tex_x - 1;
+	if (cube->rcstg.side == 1 && cube->rcstg.ray_dir_y < 0)
+		cube->rcstg.tex_x = cube->rcstg.tex->width - cube->rcstg.tex_x - 1;
+	cube->rcstg.tex_step = (double)(cube->rcstg.tex->height) / cube->rcstg.line_height;
+	cube->rcstg.tex_pos = (cube->rcstg.draw_start -
+						   cube->map_prmtrs.win_height / 2 + cube->rcstg.line_height / 2) * cube->rcstg.tex_step;
+	return (0);
+}
+
 int 	rcstng_without_textures(t_game *cube, int x)
 {
 	if (cube->plr.pos_y > cube->rcstg.map_y && cube->rcstg.side)
@@ -79,6 +100,8 @@ int 	rcstng(t_game *cube)
 		rcstng_init_2(cube);
 		rcstng_while_hit_not_zero(cube);
 		rcstng_calculations(cube);
+		rcstng_calculate_wallx(cube);
+
 		rcstng_without_textures(cube, x);
 		x++;
 	}
