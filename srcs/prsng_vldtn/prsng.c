9f9044f5a96efb6386b60e00bdaae5f4363e814d
Bellerophon Hatches <bhatches@oa-n1.msk.21-school.ru>
2021-04-05 20:42:28 +0300

    16.4 all functions for parsing ready. Now ready so do parsing. Problem with \n lines is solved.

--- srcs/prsng_vldtn/prsng.c
+++ srcs/prsng_vldtn/prsng.c
@@ -43,9 +43,7 @@ int		prsng_creat_lstmap(char *line, t_game *cube)
 
 int		prsng_fill_game(char *line, char **arr_split_line, t_game *cube)
 {
-	if (ft_memcmp(arr_split_line[0], "\n", 1) == 0)
-		printf("space line found\n");
-	else if (ft_memcmp(arr_split_line[0], "R", 1) == 0)
+	if (ft_memcmp(arr_split_line[0], "R", 1) == 0)
 		prsng_resolution(arr_split_line, cube);
 	else if (ft_memcmp(arr_split_line[0], "NO", 1) == 0)
 		prsng_no_txtr(arr_split_line, cube);
