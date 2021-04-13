85c0b89c5b05c82e74dd55744abcaf835c60f237
vpsan <valery.sandul@gmail.com>
2021-04-13 18:06:55 +0300

    17.5 Doing screenshot.

--- srcs/game/screenshot.c
+++ srcs/game/screenshot.c
@@ -1,6 +1,6 @@
 #include "game.h"
 
-int screenshot_fillzeros_bmp(unsigned char *bmp_54arr, t_game *cube)
+int screenshot_creat_bmp(unsigned char *bmp_54arr, t_game *cube, int size)
 {
 	int i;
 
@@ -9,7 +9,22 @@ int screenshot_fillzeros_bmp(unsigned char *bmp_54arr, t_game *cube)
 		bmp_54arr[i++] = (unsigned char)0;
 	bmp_54arr[0] = 'B';
 	bmp_54arr[1] = 'M';
-
+	bmp_54arr[2] = (unsigned char)(size);
+	bmp_54arr[3] = (unsigned char)(size >> 8);
+	bmp_54arr[4] = (unsigned char)(size >> 16);
+	bmp_54arr[5] = (unsigned char)(size >> 24);
+	bmp_54arr[10] = (unsigned char)54;
+	bmp_54arr[14] = (unsigned char)40;
+	bmp_54arr[18] = (unsigned char)cube->map_prmtrs.win_width;
+	bmp_54arr[19] = (unsigned char)(cube->map_prmtrs.win_width >> 8);
+	bmp_54arr[20] = (unsigned char)(cube->map_prmtrs.win_width >> 16);
+	bmp_54arr[21] = (unsigned char)(cube->map_prmtrs.win_width >> 24);
+	bmp_54arr[22] = (unsigned char)cube->map_prmtrs.win_height;
+	bmp_54arr[23] = (unsigned char)(cube->map_prmtrs.win_height >> 8);
+	bmp_54arr[24] = (unsigned char)(cube->map_prmtrs.win_height  >> 16);
+	bmp_54arr[25] = (unsigned char)(cube->map_prmtrs.win_height  >> 24);
+	bmp_54arr[26] = (unsigned char)1;
+	bmp_54arr[28] = (unsigned char)24;
 	return (0);
 }
 
@@ -24,9 +39,8 @@ int screenshot(t_game *cube)
 		my_exit(19);
 	game_init(cube);
 	loop_hook_next_frame(cube);
-	screenshot_fillzeros_bmp(bmp_54arr, cube);
-
-
+	screenshot_creat_bmp(bmp_54arr, cube, cube->map_prmtrs.win_width *
+											cube->map_prmtrs.win_height + 54);
 	for (int i; i < 54; i++)
 		printf("i = %c \n", bmp_54arr[i]);
 	printf("END screenshot\n");
